26ab2d240a07419bf3e2e2c654c55133b32b4919
Thomas Kleinendorst <thomas.kleinendorst@gmail.com>
2017-12-21 22:34:36 +0100

    Replace app with coming soon screen

---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="container">
+    <section class="main-content">
+      <div class="framed-art-text">
+        <h1>{{ msg }}</h1>
+      </div>
+      <div class="comming-soon-art">
+        <img src="../assets/art.svg" alt="comming soon illustration">
+      </div>
+    </section>
+    <footer>
+      <a href="https://www.linkedin.com/in/thomas-kleinendorst-188311144/">
+        <img class="linkedin_logo" src="../assets/linkedin_logo.svg" alt="LinkedIn">
+      </a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoon',
+  data() {
+    return {
+      msg: 'Coming soon!',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+.main-content {
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-family: 'Alegreya Sans', sans-serif;
+  text-transform: uppercase;
+  margin: .1em 0;
+  font-weight: normal;
+  font-size: 3em;
+}
+
+.framed-art-text {
+  display: inline-block;
+  border-top: 2px solid black;
+  border-bottom: 2px solid black;
+  margin: 5px;
+}
+
+.comming-soon-art {
+  width: 650px;
+  margin: 90px;
+  margin-bottom: 190px;
+}
+
+footer {
+  width: 100%;
+  height: 100px;
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+}
+
+.linkedin_logo {
+  padding: 10px;
+  margin: 0 70px;
+  display: inline-block;
+  width: 60px;
+
+  /*transition*/
+  transition: background-color 200ms;
+  border-radius: 100%;
+  background-color: rgba(0, 0, 0, 0);
+}
+
+.linkedin_logo:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+/* Time for tricks: */
+
+@media screen and (max-width: 730px) {
+  .comming-soon-art {
+    width: 90%;
+  }
+
+  .linkedin_logo {
+    margin: 0 20px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import ComingSoon from '../components/ComingSoon';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'ComingSoon',
+      component: ComingSoon,
     },
   ],
 });
