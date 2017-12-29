e90b09a9673f3b39d7dfbe576e1a2a1d5d98f260
Thomas Kleinendorst <thomas.kleinendorst@gmail.com>
2017-12-29 17:42:08 +0100

    Configure HTML5 routing in Express server and app
    Also added a 404 page to prove this.

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import ComingSoon from '../components/ComingSoon';
+import NotFound from '@/components/NotFound';
+import ComingSoon from '@/components/ComingSoon';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'ComingSoon',
       component: ComingSoon,
+    }, {
+      path: '*',
+      name: '404',
+      component: NotFound,
     },
   ],
 });
