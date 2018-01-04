66d796200bfa24837a8f97c03da4d311a5d8729a
Thomas Kleinendorst <Thomas.Kleinendorst@gmail.com>
2018-01-04 22:16:19 +0100

    Add the script used on the server for updating

---
+++ docker/cron-script.fish
@@ -0,0 +1,25 @@
+#! /usr/bin/fish
+
+# This script is used to deploy and update
+# the running Docker containers
+# This script should be run daily in crontab
+
+set WORKING_DIRECTORY /tmp/update-docker-compose-task
+set GIT_BRANCH feature-docker-compose
+
+echo "creating tmp working directory at $WORKING_DIRECTORY"
+mkdir $WORKING_DIRECTORY
+cd $WORKING_DIRECTORY
+
+echo "cloning git repo on branch: $GIT_BRANCH"
+git clone https://github.com/Kleinendorst/blog-depot.git
+cd blog-depot/
+git checkout -b $GIT_BRANCH --track remotes/origin/$GIT_BRANCH
+
+echo "run docker-compose pull & docker-compose up"
+# cd docker/$GIT_BRANCH
+cd docker/master
+docker-compose pull & docker-compose up -d
+
+echo "removing tmp working directory at $WORKING_DIRECTORY"
+rm -rf $WORKING_DIRECTORY
