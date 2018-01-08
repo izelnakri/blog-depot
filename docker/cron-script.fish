d135fc46b7b582e89366f576548b209d3d724076
Thomas Kleinendorst <thomas.kleinendorst@gmail.com>
2018-01-08 15:39:30 +0100

    Add documentation line

--- docker/cron-script.fish
+++ docker/cron-script.fish
@@ -3,6 +3,7 @@
 # This script is used to deploy and update
 # the running Docker containers
 # This script should be run daily in crontab
+# sudo crontab -e    ->    0 0 * * * [PATH_TO_SCRIPT] [BRANCH] >> [LOGFILE_PATH]
 
 set GIT_BRANCH $argv[1]
 set WORKING_DIRECTORY /tmp/update-$GIT_BRANCH
